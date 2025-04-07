5faaeaca32fd1dd15b5cbda6e6726294e96eb2df
dnjsals45 <dnjsals45@naver.com>
2025-04-07 15:25:48 +0900

    Initial Commit

---
+++ src/main/kotlin/ministory/picklab/PickLabApplication.kt
@@ -0,0 +1,11 @@
+package ministory.picklab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PickLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<PickLabApplication>(*args)
+}
---
+++ src/test/kotlin/ministory/picklab/PickLabApplicationTests.kt
@@ -0,0 +1,13 @@
+package ministory.picklab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PickLabApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
