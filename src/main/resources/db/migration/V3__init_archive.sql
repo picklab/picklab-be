cb5db748d090c4ad02af724176c77be644947aeb
dnjsals45 <dnjsals45@naver.com>
2025-05-08 17:18:00 +0900

    ✨feat: 아카이브, 리뷰, 알림 관련 스키마 작성

---
+++ src/main/resources/db/migration/V3__init_archive.sql
@@ -0,0 +1,35 @@
+-- 아카이브 테이블 생성
+-- member 테이블과 1(회원):N(아카이브) 관계
+-- activity 테이블과 1(활동):N(아카이브) 관계
+-- job_category 테이블과 N(아카이브):M(직무 카테고리) 관계
+CREATE TABLE IF NOT EXISTS archive
+(
+    id                       BIGINT         NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '아카이브 ID',
+    member_id                BIGINT         NOT NULL COMMENT '회원 ID',
+    activity_id              BIGINT         NOT NULL COMMENT '활동 ID',
+    user_start_date          DATETIME       NOT NULL COMMENT '활동 시작일',
+    user_end_date            DATETIME       NOT NULL COMMENT '활동 종료일',
+    role                     VARCHAR(50)    NOT NULL COMMENT '활동 역할 (기획 / 디자인 / 개발 / 마케팅 / AI)',
+    detail_role              VARCHAR(50)    NOT NULL COMMENT '상세 역할',
+    custom_role              VARCHAR(255)   NULL     COMMENT '상세 역할에서 기타를 선택하여 직접 입력한 역할',
+    activity_record          TEXT           NOT NULL COMMENT '활동 기록',
+    uploaded_file_url        VARCHAR(255)   NULL     COMMENT '업로드한 파일 URL',
+    reference_url            VARCHAR(255)   NULL     COMMENT '참고 URL',
+    activity_type            VARCHAR(50)    NOT NULL COMMENT '활동 구분 (대외활동 / 강연 / 세미나 / 교육 / 공모전/해커톤)',
+    activity_progress_status VARCHAR(50)    NOT NULL COMMENT '활동 진행 상태 (수료 완료 / 중도 포기)',
+    created_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
+    updated_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
+    deleted_at               DATETIME       NULL     COMMENT '삭제일'
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='아카이브 테이블';
+
+-- 아카이브 직무 관련 중간 테이블
+CREATE TABLE IF NOT EXISTS archive_job_category
+(
+    id              BIGINT   NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '아카이브 직무 ID',
+    archive_id      BIGINT   NOT NULL COMMENT '아카이브 ID',
+    job_category_id BIGINT   NOT NULL COMMENT '직무 카테고리 ID',
+    created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
+    updated_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일'
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='아카이브 직무 조 테이블';
---
+++ src/main/resources/db/migration/V4__init_review.sql
@@ -0,0 +1,23 @@
+-- 리뷰 테이블 생성
+-- member 테이블과 1(회원):N(리뷰) 관계
+-- activity 테이블과 1(활동):N(리뷰) 관계
+CREATE TABLE IF NOT EXISTS review
+(
+    id                   BIGINT         NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '리뷰 ID',
+    member_id            BIGINT         NOT NULL  COMMENT '회원 ID',
+    activity_id          BIGINT         NOT NULL  COMMENT '활동 ID',
+    overall_score        TINYINT        NOT NULL  COMMENT '총 평점',
+    info_score           TINYINT        NOT NULL  COMMENT '정보 점수',
+    difficulty_score     TINYINT        NOT NULL  COMMENT '강도 점수',
+    benefit_score        TINYINT        NOT NULL  COMMENT '혜택 점수',
+    summary              VARCHAR(255)   NOT NULL  COMMENT '한줄 평',
+    strength             VARCHAR(255)   NOT NULL  COMMENT '장점',
+    weakness             VARCHAR(255)   NOT NULL  COMMENT '단점',
+    tips                 VARCHAR(255)   NULL      COMMENT '꿀팁',
+    job_relevance_score  TINYINT        NOT NULL  COMMENT '직무 연관성 점수',
+    url                  VARCHAR(255)   NULL      COMMENT '인증 자료 URL',
+    created_at           DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
+    updated_at           DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
+    deleted_at           DATETIME       NULL      COMMENT '삭제일'
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='리뷰 테이블';
