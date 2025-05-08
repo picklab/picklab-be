3ccfaf5b7fdfb0798015f70a4874b18e9e214f8f
dnjsals45 <dnjsals45@naver.com>
2025-05-08 17:20:52 +0900

    ✨feat: 알림 관련 Entity 정의

---
+++ src/main/kotlin/picklab/backend/notification/domain/Notification.kt
@@ -0,0 +1,26 @@
+package picklab.backend.notification.domain
+
+import jakarta.persistence.*
+import org.hibernate.annotations.Comment
+import picklab.backend.common.model.SoftDeleteEntity
+import picklab.backend.member.domain.Member
+
+@Entity
+@Table(name = "notification")
+class Notification(
+    @Column(name = "title", nullable = false)
+    @Comment("알림 제목")
+    var title: String,
+    @Column(name = "type", length = 50, nullable = false)
+    @Comment("알림 타입")
+    var type: String,
+    @Column(name = "link", nullable = false)
+    @Comment("클릭 시 이동할 링크")
+    var link: String,
+    @Column(name = "is_read", nullable = false)
+    @Comment("읽음 여부")
+    var isRead: Boolean = false,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    val member: Member,
+) : SoftDeleteEntity()
