fd1cd56efb49540170ba0e6d90dd6955b0167b9c
hwk9407 <hwk__@naver.com>
2025-05-10 23:50:17 +0900

    :recycle: refactor : 활동 엔터티 타입별 상속 구조로 리펙토링
    - location, cost 컬럼을 Nullable로 변경
    - DDL 및 엔터티 주석/코멘트 정리

--- src/main/kotlin/picklab/backend/activity/domain/entity/Activity.kt
+++ src/main/kotlin/picklab/backend/activity/domain/entity/Activity.kt
@@ -1,18 +1,18 @@
 package picklab.backend.activity.domain.entity
 
 import jakarta.persistence.Column
+import jakarta.persistence.DiscriminatorColumn
+import jakarta.persistence.DiscriminatorType
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
+import jakarta.persistence.Inheritance
+import jakarta.persistence.InheritanceType
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import org.hibernate.annotations.Comment
-import picklab.backend.activity.domain.enum.ActivityFieldType
-import picklab.backend.activity.domain.enum.ActivityType
-import picklab.backend.activity.domain.enum.DomainType
-import picklab.backend.activity.domain.enum.LocationType
 import picklab.backend.activity.domain.enum.OrganizerType
 import picklab.backend.activity.domain.enum.ParticipantType
 import picklab.backend.activity.domain.enum.RecruitmentStatus
@@ -20,15 +20,16 @@ import picklab.backend.common.model.SoftDeleteEntity
 import java.time.LocalDate
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+    name = "activity_type",
+    discriminatorType = DiscriminatorType.STRING,
+)
 @Table(name = "activity")
 class Activity(
     @Column(name = "title", nullable = false, length = 50)
     @Comment("활동명")
     var title: String,
-    @Column(name = "activity_type", nullable = false, length = 50)
-    @Enumerated(EnumType.STRING)
-    @Comment("활동 유형 (대외활동, 공모전/해커톤, 강연/세미나, 교육)")
-    val activityType: ActivityType,
     @Column(name = "organizer", nullable = false, length = 50)
     @Enumerated(EnumType.STRING)
     @Comment("주최 기관/단체명")
@@ -37,10 +38,6 @@ class Activity(
     @Enumerated(EnumType.STRING)
     @Comment("참여대상")
     var targetAudience: ParticipantType,
-    @Column(name = "location", nullable = false, length = 50)
-    @Enumerated(EnumType.STRING)
-    @Comment("모임지역")
-    var location: LocationType,
     @Column(name = "recruitment_start_date", nullable = false)
     @Comment("모집 시작일")
     var recruitmentStartDate: LocalDate,
@@ -60,17 +57,6 @@ class Activity(
     @Column(name = "view_count", nullable = false)
     @Comment("조회수")
     var viewCount: Long = 0L,
-    @Column(name = "activity_field")
-    @Enumerated(EnumType.STRING)
-    @Comment("활동 분야(대외활동)")
-    var activityField: ActivityFieldType,
-    @Column(name = "domain")
-    @Enumerated(EnumType.STRING)
-    @Comment("도메인(공모전/해커톤)")
-    var domain: DomainType,
-    @Column(name = "cost", nullable = false)
-    @Comment("시상 규모(공모전/해커톤), 교육비용(교육)")
-    var cost: Long = 0, // 0: 없음
     @Column(name = "duration", nullable = false)
     @Comment("활동 기간(일)")
     var duration: Int? = -1, // -1: 무기한
