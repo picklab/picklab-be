1083b8f9f1a584c00263206c86eeb67364a6f2f8
dnjsals45 <dnjsals45@naver.com>
2025-05-12 23:57:17 +0900

    🔧build: jjwt 라이브러리(io.jsonwebtoken) 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,11 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // jjwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 kotlin {
