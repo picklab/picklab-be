fd02cf6a24c8c2d1e7ea86c99676e99853eeeb7c
dnjsals45 <dnjsals45@naver.com>
2025-05-13 00:08:32 +0900

    ✨feat: 멤버 데이터 프로필 이미지 url, 고용 형태, 탈퇴 사유, 탈퇴 상세 사유 컬럼 추가

--- src/main/kotlin/picklab/backend/member/domain/Member.kt
+++ src/main/kotlin/picklab/backend/member/domain/Member.kt
@@ -29,6 +29,9 @@ class Member(
     @Column(name = "nickname", nullable = false, length = 50)
     @Comment("닉네임")
     var nickname: String = "",
+    @Column(name = "profile_image_url", length = 255)
+    @Comment("프로필 이미지 URL")
+    var profileImageUrl: String = "",
     @Column(name = "education_level", nullable = false, length = 50)
     @Comment("최종 학력")
     var educationLevel: String = "",
@@ -38,9 +41,18 @@ class Member(
     @Column(name = "employment_status", nullable = false, length = 50)
     @Comment("재직 상태")
     var employmentStatus: String = "",
+    @Column(name = "employment_type", length = 50)
+    @Comment("고용 형태")
+    var employmentType: String = "",
     @Column(name = "is_completed", nullable = false)
     @Comment("회원 가입 완료 여부")
     var isCompleted: Boolean = false,
+    @Column(name = "withdrawal_reason", length = 255)
+    @Comment("탈퇴 사유")
+    var withdrawalReason: String? = null,
+    @Column(name = "withdrawal_reason_detail", length = 255)
+    @Comment("탈퇴 상세 사유")
+    var withdrawalReasonDetail: String? = null,
     @OneToMany(mappedBy = "member", cascade = [CascadeType.PERSIST], fetch = FetchType.LAZY)
     val socialLogins: MutableList<SocialLogin> = mutableListOf(),
     @OneToMany(mappedBy = "member", cascade = [CascadeType.PERSIST], fetch = FetchType.LAZY)
--- src/main/resources/db/migration/V1__init_member.sql
+++ src/main/resources/db/migration/V1__init_member.sql
@@ -1,21 +1,25 @@
 -- Picklab 회원 테이블 생성
 CREATE TABLE IF NOT EXISTS member
 (
-    id                BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '회원 ID',
-    name              VARCHAR(20)  NOT NULL COMMENT '회원 이름',
-    email             VARCHAR(100) NOT NULL COMMENT '회원 이메일',
-    company           VARCHAR(50)  NOT NULL COMMENT '재직중인 회사',
-    school            VARCHAR(50)  NOT NULL COMMENT '최종 학교',
-    department        VARCHAR(50)  NOT NULL COMMENT '전공',
-    birth_date        DATE         NULL COMMENT '생년월일',
-    nickname          VARCHAR(50)  NOT NULL COMMENT '닉네임',
-    education_level   VARCHAR(50)  NOT NULL COMMENT '최종 학력',
-    gradation_status  VARCHAR(50)  NOT NULL COMMENT '학업 상태',
-    employment_status VARCHAR(50)  NOT NULL COMMENT '재직 상태',
-    is_completed      BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '회원 가입 완료 여부',
-    created_at        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
-    updated_at        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
-    deleted_at        DATETIME     NULL COMMENT '삭제일'
+    id                       BIGINT        NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '회원 ID',
+    name                     VARCHAR(20)   NOT NULL COMMENT '회원 이름',
+    email                    VARCHAR(100)  NOT NULL COMMENT '회원 이메일',
+    company                  VARCHAR(50)   NOT NULL COMMENT '재직중인 회사',
+    school                   VARCHAR(50)   NOT NULL COMMENT '최종 학교',
+    department               VARCHAR(50)   NOT NULL COMMENT '전공',
+    birth_date               DATE          NULL COMMENT '생년월일',
+    nickname                 VARCHAR(50)   NOT NULL COMMENT '닉네임',
+    profile_image_url        VARCHAR(255)  NULL COMMENT '프로필 이미지 url',
+    education_level          VARCHAR(50)   NOT NULL COMMENT '최종 학력',
+    gradation_status         VARCHAR(50)   NOT NULL COMMENT '학업 상태',
+    employment_status        VARCHAR(50)   NOT NULL COMMENT '재직 상태',
+    employment_type          VARCHAR(50)   NULL COMMENT '고용 형태',
+    is_completed             BOOLEAN       NOT NULL DEFAULT FALSE COMMENT '회원 가입 완료 여부',
+    withdrawal_reason        VARCHAR(255)  NULL COMMENT '탈퇴 사유',
+    withdrawal_reason_detail VARCHAR(2000) NULL COMMENT '탈퇴 상세 사유',
+    created_at               DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
+    updated_at               DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
+    deleted_at               DATETIME      NULL COMMENT '삭제일'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='회원 테이블';
 
