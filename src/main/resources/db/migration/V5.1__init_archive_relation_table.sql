81e84b8b4c69087bb32b9becf06ed288af05220f
dnjsals45 <dnjsals45@naver.com>
2025-05-14 02:29:13 +0900

    🐛fix: 스키마 오타 수정

--- src/main/resources/db/migration/V5.1__init_archive_relation_table.sql
+++ src/main/resources/db/migration/V5.1__init_archive_relation_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS archive_reference_url
     archive_id               BIGINT         NOT NULL COMMENT '아카이브 ID',
     url                      VARCHAR(2084)  NOT NULL COMMENT '참고 URL',
     created_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
-    updated_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
+    updated_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='아카이브 참고 URL 테이블';
 
@@ -18,6 +18,6 @@ CREATE TABLE IF NOT EXISTS archive_upload_file_url
     archive_id               BIGINT         NOT NULL COMMENT '아카이브 ID',
     url                      VARCHAR(2084)  NOT NULL COMMENT '업로드 파일 URL',
     created_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
-    updated_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
+    updated_at               DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='아카이브 업로드 파일 URL 테이블';
