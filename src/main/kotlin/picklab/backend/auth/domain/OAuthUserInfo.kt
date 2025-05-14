ab27a3432ba786c4b698af1e276d6a752b233817
dnjsals45 <dnjsals45@naver.com>
2025-05-15 00:05:16 +0900

    ✨feat: 소셜 로그인 유저 정보 관련 인터페이스 정의

---
+++ src/main/kotlin/picklab/backend/auth/domain/OAuthUserInfo.kt
@@ -0,0 +1,15 @@
+package picklab.backend.auth.domain
+
+import java.time.LocalDate
+
+interface OAuthUserInfo {
+    fun getSocialId(): String
+
+    fun getName(): String?
+
+    fun getEmail(): String?
+
+    fun getProfileImage(): String?
+
+    fun getBirthdate(): LocalDate?
+}
