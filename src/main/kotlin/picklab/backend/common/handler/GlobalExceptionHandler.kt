d75efc8233cb425da93a05b5baa5fa66e29d055c
yongckim <zxcv9203@naver.com>
2025-05-20 23:51:45 +0900

    :sparkles: feat : 공통 예외 핸들링 객체 추가

---
+++ src/main/kotlin/picklab/backend/common/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,72 @@
+package picklab.backend.common.handler
+
+import org.springframework.beans.ConversionNotSupportedException
+import org.springframework.beans.TypeMismatchException
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.http.converter.HttpMessageNotWritableException
+import org.springframework.validation.method.MethodValidationException
+import org.springframework.web.ErrorResponseException
+import org.springframework.web.HttpMediaTypeNotAcceptableException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingPathVariableException
+import org.springframework.web.bind.MissingServletRequestParameterException
+import org.springframework.web.bind.ServletRequestBindingException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.async.AsyncRequestNotUsableException
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException
+import org.springframework.web.method.annotation.HandlerMethodValidationException
+import org.springframework.web.multipart.MaxUploadSizeExceededException
+import org.springframework.web.multipart.support.MissingServletRequestPartException
+import org.springframework.web.servlet.NoHandlerFoundException
+import org.springframework.web.servlet.resource.NoResourceFoundException
+import picklab.backend.common.model.ErrorCode
+import picklab.backend.common.model.ResponseWrapper
+import picklab.backend.common.util.logger
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    val log = logger()
+
+    @ExceptionHandler(
+        HttpRequestMethodNotSupportedException::class,
+        HttpMediaTypeNotSupportedException::class,
+        HttpMediaTypeNotAcceptableException::class,
+        MissingPathVariableException::class,
+        MissingServletRequestParameterException::class,
+        MissingServletRequestPartException::class,
+        ServletRequestBindingException::class,
+        MethodArgumentNotValidException::class,
+        HandlerMethodValidationException::class,
+        NoHandlerFoundException::class,
+        NoResourceFoundException::class,
+        AsyncRequestTimeoutException::class,
+        ErrorResponseException::class,
+        MaxUploadSizeExceededException::class,
+        ConversionNotSupportedException::class,
+        TypeMismatchException::class,
+        HttpMessageNotReadableException::class,
+        HttpMessageNotWritableException::class,
+        MethodValidationException::class,
+        AsyncRequestNotUsableException::class,
+    )
+    fun handleMvcException(e: Exception): ResponseEntity<ResponseWrapper<Unit>> {
+        log.warn("[handleMvcException] ${e.message}", e)
+
+        return ResponseEntity
+            .status(ErrorCode.BAD_REQUEST.status)
+            .body(ResponseWrapper.error(ErrorCode.BAD_REQUEST))
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ResponseEntity<ResponseWrapper<Unit>> {
+        log.error("[handleException] ${e.message}", e)
+
+        return ResponseEntity
+            .status(ErrorCode.INTERNAL_SERVER_ERROR.status)
+            .body(ResponseWrapper.error(ErrorCode.INTERNAL_SERVER_ERROR))
+    }
+}
