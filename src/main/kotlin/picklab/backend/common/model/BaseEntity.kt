7037e3b66044a125cc40f88f5fa4d7c09a5852bc
dnjsals45 <dnjsals45@naver.com>
2025-05-20 14:24:46 +0900

    ♻️refactor: JPA 생성 문제없도록 변경

--- src/main/kotlin/picklab/backend/common/model/BaseEntity.kt
+++ src/main/kotlin/picklab/backend/common/model/BaseEntity.kt
@@ -10,19 +10,16 @@ import java.time.LocalDateTime
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity(
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     val id: Long = 0,
-
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     @Comment("생성 시간")
-    val createdAt: LocalDateTime = LocalDateTime.MIN,
-
+    var createdAt: LocalDateTime = LocalDateTime.MIN,
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     @Comment("수정 시간")
-    val updatedAt: LocalDateTime = LocalDateTime.MIN
+    var updatedAt: LocalDateTime = LocalDateTime.MIN,
 )
