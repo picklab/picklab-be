7cb11f5e231b03bc950b704b1b0568d4894c005e
yongckim <zxcv9203@naver.com>
2025-05-26 22:09:21 +0900

    :recycle: refactor : 성공 응답에 경우 SuccessCode를 이용하도록 변경

--- src/main/kotlin/picklab/backend/common/model/SuccessCode.kt
+++ src/main/kotlin/picklab/backend/common/model/SuccessCode.kt
@@ -6,5 +6,6 @@ enum class SuccessCode(
     val status: HttpStatus,
     val message: String,
 ) {
+    SOCIAL_LOGIN_SUCCESS(HttpStatus.OK, "소셜 로그인 성공"),
     JOB_DETAILS_RETRIEVED(HttpStatus.OK, "직무 상세 목록 조회에 성공했습니다."),
 }
