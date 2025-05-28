0d7ddd8fbabf4a076fcdcb7972a5a9e2772aa725
dnjsals45 <dnjsals45@naver.com>
2025-05-28 19:01:10 +0900

    🔧config: jjwt 라이브러리 버전 변수화

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 group = "picklab"
 version = "0.0.1-SNAPSHOT"
 val swaggerVersion = "2.8.8"
+val jjwtVersion = "0.12.6"
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
@@ -45,9 +46,9 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // jjwt
-    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
 }
 
 kotlin {
