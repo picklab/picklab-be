d57c1b1af96f62e6bec1d359a126a924595b530e
dnjsals45 <dnjsals45@naver.com>
2025-06-04 20:39:05 +0900

    🔧config: 메일 발송을 위한 의존성 추가 및 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
 
     // jjwt
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
