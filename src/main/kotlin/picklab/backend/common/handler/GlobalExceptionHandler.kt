74971bcfb7ff91254f570a24744de647d3eed7ad
dnjsals45 <dnjsals45@naver.com>
2025-06-18 00:49:47 +0900

    ♻️refactor: 에러 응답 형식 변경 및 추적 가능한 데이터 추가

--- src/main/kotlin/picklab/backend/common/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/picklab/backend/common/handler/GlobalExceptionHandler.kt
@@ -1,10 +1,10 @@
 package picklab.backend.common.handler
 
+import com.fasterxml.jackson.databind.JsonMappingException
 import org.springframework.beans.ConversionNotSupportedException
 import org.springframework.beans.TypeMismatchException
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
-import org.springframework.validation.method.MethodValidationException
 import org.springframework.web.ErrorResponseException
 import org.springframework.web.HttpMediaTypeNotAcceptableException
 import org.springframework.web.HttpMediaTypeNotSupportedException
@@ -23,6 +23,8 @@ import org.springframework.web.servlet.resource.NoResourceFoundException
 import picklab.backend.auth.domain.AuthException
 import picklab.backend.common.model.BusinessException
 import picklab.backend.common.model.ErrorCode
+import picklab.backend.common.model.ErrorField
+import picklab.backend.common.model.ErrorResponseWrapper
 import picklab.backend.common.model.ResponseWrapper
 import picklab.backend.common.util.logger
 
@@ -38,33 +40,89 @@ class GlobalExceptionHandler {
         MissingServletRequestParameterException::class,
         MissingServletRequestPartException::class,
         ServletRequestBindingException::class,
-        MethodArgumentNotValidException::class,
         HandlerMethodValidationException::class,
         ErrorResponseException::class,
         MaxUploadSizeExceededException::class,
         ConversionNotSupportedException::class,
         TypeMismatchException::class,
-        HttpMessageNotReadableException::class,
-        MethodValidationException::class,
     )
-    fun handleMvcException(e: Exception): ResponseEntity<ResponseWrapper<Unit>> {
+    fun handleMvcException(e: Exception): ResponseEntity<ErrorResponseWrapper> {
         log.warn("[handleMvcException] ${e.message}", e)
 
         return ResponseEntity
             .status(ErrorCode.BAD_REQUEST.status)
-            .body(ResponseWrapper.error(ErrorCode.BAD_REQUEST))
+            .body(ErrorResponseWrapper.error(ErrorCode.BAD_REQUEST))
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponseWrapper> {
+        log.warn("[handleMethodValidationException] ${e.message}", e)
+
+        val errors =
+            e.bindingResult.fieldErrors.map { fieldError ->
+                ErrorField(
+                    field = fieldError.field,
+                    message = fieldError.defaultMessage,
+                )
+            }
+
+        return ResponseEntity
+            .status(ErrorCode.BAD_REQUEST.status)
+            .body(
+                ErrorResponseWrapper.error(
+                    code = ErrorCode.BAD_REQUEST,
+                    message = "요청 검증에 실패했습니다.",
+                    errors = errors,
+                ),
+            )
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<ErrorResponseWrapper> {
+        log.warn("[handleHttpMessageNotReadableException] ${e.message}", e)
+
+        if (e.cause is JsonMappingException) {
+            val mappingException = e.cause as JsonMappingException
+
+            val errors =
+                mappingException.path
+                    .mapNotNull { path ->
+                        ErrorField(
+                            field = path.fieldName,
+                            message = "${path.fieldName} 필드가 올바르지 않습니다.",
+                        )
+                    }
+
+            return ResponseEntity
+                .status(ErrorCode.BAD_REQUEST.status)
+                .body(
+                    ErrorResponseWrapper.error(
+                        code = ErrorCode.BAD_REQUEST,
+                        message = "요청 검증에 실패했습니다.",
+                        errors = errors,
+                    ),
+                )
+        }
+
+        return ResponseEntity
+            .status(ErrorCode.BAD_REQUEST.status)
+            .body(
+                ErrorResponseWrapper.error(
+                    code = ErrorCode.BAD_REQUEST,
+                ),
+            )
     }
 
     @ExceptionHandler(
         NoHandlerFoundException::class,
         NoResourceFoundException::class,
     )
-    fun handleMvcNotFoundException(e: Exception): ResponseEntity<ResponseWrapper<Unit>> {
+    fun handleMvcNotFoundException(e: Exception): ResponseEntity<ErrorResponseWrapper> {
         log.warn("[handleNotFoundExceptions] ${e.message}", e)
 
         return ResponseEntity
             .status(ErrorCode.NOT_FOUND.status)
-            .body(ResponseWrapper.error(ErrorCode.NOT_FOUND))
+            .body(ErrorResponseWrapper.error(ErrorCode.NOT_FOUND))
     }
 
     @ExceptionHandler(Exception::class)
@@ -77,20 +135,20 @@ class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(AuthException::class)
-    fun handleAuthException(e: AuthException): ResponseEntity<ResponseWrapper<Unit>> {
+    fun handleAuthException(e: AuthException): ResponseEntity<ErrorResponseWrapper> {
         log.warn("[handleAuthException] ${e.message}", e)
 
         return ResponseEntity
             .status(e.errorCode.status)
-            .body(ResponseWrapper.error(e.errorCode))
+            .body(ErrorResponseWrapper.error(e.errorCode))
     }
 
     @ExceptionHandler(BusinessException::class)
-    fun handleBusinessException(e: BusinessException): ResponseEntity<ResponseWrapper<Unit>> {
+    fun handleBusinessException(e: BusinessException): ResponseEntity<ErrorResponseWrapper> {
         log.warn("[handleBusinessException] ${e.message}", e)
 
         return ResponseEntity
             .status(e.errorCode.status)
-            .body(ResponseWrapper.error(e.errorCode))
+            .body(ErrorResponseWrapper.error(e.errorCode))
     }
 }
