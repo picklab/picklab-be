0d9de07ef1fddea2b62788bf5c69539e8c2eaaeb
dnjsals45 <dnjsals45@naver.com>
2025-06-27 16:36:27 +0900

    ✨feat: 테이블 간 매핑 설정

--- src/main/kotlin/picklab/backend/activity/domain/entity/ActivityUploadFile.kt
+++ src/main/kotlin/picklab/backend/activity/domain/entity/ActivityUploadFile.kt
@@ -2,6 +2,9 @@ package picklab.backend.activity.domain.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import org.hibernate.annotations.Comment
 import picklab.backend.common.model.BaseEntity
@@ -15,4 +18,8 @@ class ActivityUploadFile(
     @Column(name = "url", nullable = false, length = 2084)
     @Comment("파일 URL")
     var url: String,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", nullable = false)
+    @Comment("활동 ID")
+    var activity: Activity,
 ) : BaseEntity()
