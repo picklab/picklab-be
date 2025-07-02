5843f8af28d3d52c807cf7ccbdc73ac8949c579a
dnjsals45 <dnjsals45@naver.com>
2025-07-02 22:21:11 +0900

    ✨feat: 활동 북마크 생성/해제 기능 구현

--- src/main/kotlin/picklab/backend/activity/application/ActivityUseCase.kt
+++ src/main/kotlin/picklab/backend/activity/application/ActivityUseCase.kt
@@ -67,4 +67,6 @@ class ActivityUseCase(
             isBookmarked = isBookmarked,
         )
     }
+
+    fun mustFindActivity(activityId: Long) = activityService.mustFindActiveActivity(activityId)
 }
