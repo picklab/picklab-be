0403a0b51365750b0cd288305ebf9b5b91e81c04
tianea <rhlehfndvkd7557@gmail.com>
2025-07-05 16:16:50 +0900

    Refactor : 누락된 Schema 적용 및 Companion Object에 대해서 예외처리

--- src/main/kotlin/picklab/backend/archive/entrypoint/request/ArchiveUpdateRequest.kt
+++ src/main/kotlin/picklab/backend/archive/entrypoint/request/ArchiveUpdateRequest.kt
@@ -1,9 +1,12 @@
 package picklab.backend.archive.entrypoint.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import picklab.backend.archive.domain.enums.PassOrFailStatus
 import picklab.backend.archive.domain.enums.ProgressStatus
 
 class ArchiveUpdateRequest(
+    @field:Schema(description = "활동 진행 상태")
     val activityProgressStatus: ProgressStatus,
+    @field:Schema(description = "합격/불합격 상태")
     val passOrFailStatus: PassOrFailStatus,
 )
--- src/main/kotlin/picklab/backend/member/entrypoint/response/GetSocialLoginsResponse.kt
+++ src/main/kotlin/picklab/backend/member/entrypoint/response/GetSocialLoginsResponse.kt
@@ -1,5 +1,8 @@
 package picklab.backend.member.entrypoint.response
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class GetSocialLoginsResponse(
+    @field:Schema(description = "로그인 유형")
     val loginType: List<String>,
 )
