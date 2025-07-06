ba7407b0571d877fc0355c50c55244cc34d33eee
tianea <rhlehfndvkd7557@gmail.com>
2025-07-06 14:25:23 +0900

    Refactor : Kotlin Reflection을 이용해서 Schema Annotation 적용하기

--- src/main/kotlin/picklab/backend/member/entrypoint/request/UpdateProfileImageRequest.kt
+++ src/main/kotlin/picklab/backend/member/entrypoint/request/UpdateProfileImageRequest.kt
@@ -7,6 +7,6 @@ import jakarta.validation.constraints.NotBlank
 data class UpdateProfileImageRequest(
     @field:NotBlank(message = "프로필 이미지 URL은 필수 입력값입니다.")
     @field:JsonProperty("profile_image")
-    @field:Schema(description = "프로필 이미지 URL", example = "https://example.com/profile.jpg")
+    @Schema(description = "프로필 이미지 URL", example = "https://example.com/profile.jpg")
     val profileImage: String,
 )
