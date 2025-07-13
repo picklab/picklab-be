15ca47a6662c5a7d8c5ab84ed9c9164f91dbceb4
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-07-13 09:58:15 +0900

    ♻️refactor: 이메일 전송 기능 비동기 작업으로 변경 (#33)
    * ♻️refactor: 이메일 인증 코드 생성 서비스 분리
    
    * 🔧config: AsyncConfig 추가
    
    * ✨feat: 이메일 전송 기능 @Async 어노테이션 추가

--- src/main/kotlin/picklab/backend/member/infrastructure/MailService.kt
+++ src/main/kotlin/picklab/backend/member/infrastructure/MailService.kt
@@ -2,13 +2,14 @@ package picklab.backend.member.infrastructure
 
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
-import java.security.SecureRandom
 
 @Service
 class MailService(
     private val mailSender: JavaMailSender,
 ) {
+    @Async
     fun sendMail(
         target: String,
         code: String,
@@ -18,18 +19,14 @@ class MailService(
         helper.setTo(target)
         helper.setSubject("PickLab 이메일 인증 코드")
         helper.setText(
-            "PickLab 이메일 인증 코드입니다. 인증 코드는 $code 입니다. " +
-                "인증 코드를 입력하여 이메일 인증을 완료해주세요. 코드는 5분간 유효합니다.",
-            true,
+            """
+            PickLab 이메일 인증 코드입니다.
+            인증 코드는 $code 입니다.
+            인증 코드를 입력하여 이메일 인증을 완료해주세요.
+            코드는 5분간 유효합니다.
+            """.trimIndent(),
+            false,
         )
         mailSender.send(message)
-    }
-
-    fun createVerificationCode(): String {
-        val secureRandom = SecureRandom()
-
-        val code = secureRandom.nextInt(1000000).toString()
-
-        return code.padStart(6, '0')
     }
 }
