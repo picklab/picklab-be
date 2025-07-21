dd532c6dddc3261c579fc4ebe24e0f9008cc10d5
dnjsals45 <dnjsals45@naver.com>
2025-07-21 16:03:41 +0900

    ✨feat: 활동 지원 기능 구현

--- src/main/kotlin/picklab/backend/common/model/SuccessCode.kt
+++ src/main/kotlin/picklab/backend/common/model/SuccessCode.kt
@@ -29,6 +29,7 @@ enum class SuccessCode(
     ACTIVITY_BOOKMARK_CREATED(HttpStatus.CREATED, "북마크가 추가되었습니다."),
     ACTIVITY_BOOKMARK_REMOVED(HttpStatus.OK, "북마크가 삭제되었습니다."),
     INCREASE_VIEW_COUNT(HttpStatus.OK, "조회수 증가에 성공했습니다."),
+    APPLY_ACTIVITY(HttpStatus.OK, "활동 지원에 성공했습니다."),
 
     // Activity Participation 도메인 관련
     GET_ACTIVITY_APPLICATION_URL(HttpStatus.OK, "활동 지원 링크를 가져오는데 성공했습니다."),
