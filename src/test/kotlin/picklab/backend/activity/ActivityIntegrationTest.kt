a7e05a77703d982c6ca1ca9f1c69c285e2baa333
dnjsals45 <dnjsals45@naver.com>
2025-07-26 13:36:00 +0900

    ✅test: 사용자 직무 관련 활동 추천 테스트 작성

--- src/test/kotlin/picklab/backend/activity/ActivityIntegrationTest.kt
+++ src/test/kotlin/picklab/backend/activity/ActivityIntegrationTest.kt
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.post
 import picklab.backend.activity.application.ViewCountLimiterPort.Companion.MAX_VIEW_ATTEMPTS
 import picklab.backend.activity.domain.entity.ActivityBookmark
 import picklab.backend.activity.domain.entity.ActivityGroup
+import picklab.backend.activity.domain.entity.ActivityJobCategory
 import picklab.backend.activity.domain.entity.CompetitionActivity
 import picklab.backend.activity.domain.entity.EducationActivity
 import picklab.backend.activity.domain.entity.ExternalActivity
@@ -26,6 +27,7 @@ import picklab.backend.activity.domain.enums.ParticipantType
 import picklab.backend.activity.domain.enums.RecruitmentStatus
 import picklab.backend.activity.domain.repository.ActivityBookmarkRepository
 import picklab.backend.activity.domain.repository.ActivityGroupRepository
+import picklab.backend.activity.domain.repository.ActivityJobCategoryRepository
 import picklab.backend.activity.domain.repository.ActivityRepository
 import picklab.backend.activity.entrypoint.response.GetActivityDetailResponse
 import picklab.backend.activity.entrypoint.response.GetActivityListResponse
@@ -33,7 +35,12 @@ import picklab.backend.common.model.ResponseWrapper
 import picklab.backend.common.model.SuccessCode
 import picklab.backend.helper.WithMockUser
 import picklab.backend.helper.extractBody
+import picklab.backend.job.domain.JobCategoryRepository
+import picklab.backend.job.domain.entity.JobCategory
+import picklab.backend.job.domain.enums.JobGroup
+import picklab.backend.member.domain.entity.InterestedJobCategory
 import picklab.backend.member.domain.entity.Member
+import picklab.backend.member.domain.repository.InterestedJobCategoryRepository
 import picklab.backend.member.domain.repository.MemberRepository
 import picklab.backend.template.IntegrationTest
 import java.time.LocalDate
@@ -51,6 +58,15 @@ class ActivityIntegrationTest : IntegrationTest() {
 
     @Autowired
     lateinit var activityBookmarkRepository: ActivityBookmarkRepository
+
+    @Autowired
+    lateinit var activityJobCategoryRepository: ActivityJobCategoryRepository
+
+    @Autowired
+    lateinit var jobCategoryRepository: JobCategoryRepository
+
+    @Autowired
+    lateinit var interestedJobCategoryRepository: InterestedJobCategoryRepository
 
     @Autowired
     lateinit var cacheManager: CacheManager
@@ -1210,7 +1226,162 @@ class ActivityIntegrationTest : IntegrationTest() {
                 assertThat(items[0].isBookmarked).isTrue
             }
         }
-    }
+
+        @Nested
+        @DisplayName("홈 화면에 관련된 활동 리스트를 조회한다")
+        inner class GetHomeActivitiesTest {
+            @Test
+            @WithMockUser
+            @DisplayName("[성공] 유저 기반 직무 추천 활동 리스트를 조회한다")
+            fun getUserJobRecommendedActivitiesTest() {
+                // Given
+                val member =
+                    memberRepository.save(
+                        Member(
+                            name = "테스트 유저",
+                            email = "test@example.com",
+                        ),
+                    )
+
+                // 유저 관심 직무 설정 (개발, 기획)
+                val developmentJobCategory =
+                    jobCategoryRepository.save(
+                        JobCategory(jobGroup = JobGroup.DEVELOPMENT),
+                    )
+                val planningJobCategory =
+                    jobCategoryRepository.save(
+                        JobCategory(jobGroup = JobGroup.PLANNING),
+                    )
+                val marketingJobCategory =
+                    jobCategoryRepository.save(
+                        JobCategory(jobGroup = JobGroup.MARKETING),
+                    )
+
+                interestedJobCategoryRepository.save(
+                    InterestedJobCategory(member = member, jobCategory = developmentJobCategory),
+                )
+                interestedJobCategoryRepository.save(
+                    InterestedJobCategory(member = member, jobCategory = planningJobCategory),
+                )
+
+                // 유저 관심 직무 기반 활동 2개 생성
+                val interestedActivity1 =
+                    activityRepository.save(
+                        ExternalActivity(
+                            title = "관심 직무 활동 1",
+                            organizer = OrganizerType.PUBLIC_ORGANIZATION,
+                            targetAudience = ParticipantType.WORKER,
+                            location = LocationType.SEOUL_INCHEON,
+                            recruitmentStartDate = LocalDate.now().plusDays(1),
+                            recruitmentEndDate = LocalDate.now().plusMonths(1),
+                            startDate = LocalDate.now().plusMonths(3),
+                            endDate = LocalDate.now().plusMonths(6),
+                            status = RecruitmentStatus.OPEN,
+                            viewCount = 10L,
+                            duration = 90,
+                            activityThumbnailUrl = null,
+                            activityGroup = activityGroup,
+                            activityField = ActivityFieldType.MENTORING,
+                            benefit = "",
+                        ),
+                    )
+
+                val interestedActivity2 =
+                    activityRepository.save(
+                        ExternalActivity(
+                            title = "관심 직무 활동 2",
+                            organizer = OrganizerType.PUBLIC_ORGANIZATION,
+                            targetAudience = ParticipantType.WORKER,
+                            location = LocationType.SEOUL_INCHEON,
+                            recruitmentStartDate = LocalDate.now().plusDays(1),
+                            recruitmentEndDate = LocalDate.now().plusMonths(1),
+                            startDate = LocalDate.now().plusMonths(3),
+                            endDate = LocalDate.now().plusMonths(6),
+                            status = RecruitmentStatus.OPEN,
+                            viewCount = 10L,
+                            duration = 90,
+                            activityThumbnailUrl = null,
+                            activityGroup = activityGroup,
+                            activityField = ActivityFieldType.MENTORING,
+                            benefit = "",
+                        ),
+                    )
+
+                // 활동과 직무 카테고리 연결
+                activityJobCategoryRepository.save(
+                    ActivityJobCategory(activity = interestedActivity1, jobCategory = developmentJobCategory),
+                )
+                activityJobCategoryRepository.save(
+                    ActivityJobCategory(activity = interestedActivity2, jobCategory = planningJobCategory),
+                )
+
+                // activity2 북마크 추가
+                activityBookmarkRepository.save(
+                    ActivityBookmark(member = member, activity = interestedActivity2),
+                )
+                activityBookmarkRepository.save(
+                    ActivityBookmark(
+                        member = memberRepository.save(Member(name = "테스트 유저 1", email = "test1@example.com")),
+                        activity = interestedActivity2,
+                    ),
+                )
+                activityBookmarkRepository.save(
+                    ActivityBookmark(
+                        member = memberRepository.save(Member(name = "테스트 유저 2", email = "test2@example.com")),
+                        activity = interestedActivity2,
+                    ),
+                )
+
+                // 유저가 관심없는 직무 기반 활동 1개 생성
+                val nonInterestedActivity =
+                    activityRepository.save(
+                        ExternalActivity(
+                            title = "관심없는 직무 활동",
+                            organizer = OrganizerType.PUBLIC_ORGANIZATION,
+                            targetAudience = ParticipantType.WORKER,
+                            location = LocationType.SEOUL_INCHEON,
+                            recruitmentStartDate = LocalDate.now().plusDays(1),
+                            recruitmentEndDate = LocalDate.now().plusMonths(1),
+                            startDate = LocalDate.now().plusMonths(3),
+                            endDate = LocalDate.now().plusMonths(6),
+                            status = RecruitmentStatus.OPEN,
+                            viewCount = 200L,
+                            duration = 90,
+                            activityThumbnailUrl = null,
+                            activityGroup = activityGroup,
+                            activityField = ActivityFieldType.MENTORING,
+                            benefit = "",
+                        ),
+                    )
+
+                activityJobCategoryRepository.save(
+                    ActivityJobCategory(activity = nonInterestedActivity, jobCategory = marketingJobCategory),
+                )
+
+                // When - 데이터 1개만 조회되도록 mockMvc 설정
+                val result =
+                    mockMvc
+                        .get("/v1/activities/recommendations") {
+                            param("size", "1")
+                            param("page", "1")
+                        }.andExpect { status { isOk() } }
+                        .andExpect { jsonPath("$.code") { value(SuccessCode.GET_ACTIVITIES.status.value()) } }
+                        .andExpect { jsonPath("$.message") { value(SuccessCode.GET_ACTIVITIES.message) } }
+                        .andReturn()
+
+                val body: ResponseWrapper<GetActivityListResponse> = result.extractBody(mapper)
+                val got = body.data!!
+
+                // Then - 관심 직무 기반 활동 2개중, 북마크 수 + 조회수가 높은 활동을 가져오는지 체크
+                val items = got.items
+                assertThat(got.page).isEqualTo(1)
+                assertThat(got.size).isEqualTo(1)
+                assertThat(got.totalElements).isEqualTo(2)
+                assertThat(got.totalPages).isEqualTo(2)
+                assertThat(items[0].id).isEqualTo(interestedActivity2.id)
+                assertThat(items[0].isBookmarked).isTrue
+            }
+        }
 
     @Nested
     @DisplayName("활동 상세 조회 통합 테스트")
@@ -1246,52 +1417,52 @@ class ActivityIntegrationTest : IntegrationTest() {
                     ),
                 )
 
-            val member =
-                memberRepository.save(
-                    Member(
-                        name = "기존유저",
-                        email = "test@example.com",
+                val member =
+                    memberRepository.save(
+                        Member(
+                            name = "기존유저",
+                            email = "test@example.com",
+                        ),
+                    )
+
+                activityBookmarkRepository.save(
+                    ActivityBookmark(
+                        member = member,
+                        activity = given,
                     ),
                 )
 
-            activityBookmarkRepository.save(
-                ActivityBookmark(
-                    member = member,
-                    activity = given,
-                ),
-            )
+                // When
+                val result =
+                    mockMvc
+                        .get("/v1/activities/${given.id}")
+                        .andExpect { status { isOk() } }
+                        .andExpect { jsonPath("$.code") { value(SuccessCode.GET_ACTIVITY_DETAIL.status.value()) } }
+                        .andExpect { jsonPath("$.message") { value(SuccessCode.GET_ACTIVITY_DETAIL.message) } }
+                        .andReturn()
 
-            // When
-            val result =
-                mockMvc
-                    .get("/v1/activities/${given.id}")
-                    .andExpect { status { isOk() } }
-                    .andExpect { jsonPath("$.code") { value(SuccessCode.GET_ACTIVITY_DETAIL.status.value()) } }
-                    .andExpect { jsonPath("$.message") { value(SuccessCode.GET_ACTIVITY_DETAIL.message) } }
-                    .andReturn()
+                val body: ResponseWrapper<GetActivityDetailResponse> = result.extractBody(mapper)
+                val got = body.data!!
 
-            val body: ResponseWrapper<GetActivityDetailResponse> = result.extractBody(mapper)
-            val got = body.data!!
-
-            // Then
-            assertThat(got).isNotNull
-            assertThat(got.id).isEqualTo(given.id)
-            assertThat(got.title).isEqualTo(given.title)
-            assertThat(got.category).isEqualTo(ActivityType.EXTRACURRICULAR.name)
-            assertThat(got.domains).isEmpty()
-            assertThat(got.regions).isEqualTo(listOf("서울", "인천"))
-            assertThat(got.benefits).isEqualTo("테스트 혜택")
-            assertThat(got.isBookmarked).isTrue
+                // Then
+                assertThat(got).isNotNull
+                assertThat(got.id).isEqualTo(given.id)
+                assertThat(got.title).isEqualTo(given.title)
+                assertThat(got.category).isEqualTo(ActivityType.EXTRACURRICULAR.name)
+                assertThat(got.domains).isEmpty()
+                assertThat(got.regions).isEqualTo(listOf("서울", "인천"))
+                assertThat(got.benefits).isEqualTo("테스트 혜택")
+                assertThat(got.isBookmarked).isTrue
+            }
         }
-    }
 
-    @Nested
-    @DisplayName("활동 조회 수 증가 중복 방지 테스트")
-    inner class IncreaseViewCountTest {
-        @BeforeEach
-        fun cacheClear() {
-            cacheManager.getCache("activityViewCount")?.clear()
-        }
+        @Nested
+        @DisplayName("활동 조회 수 증가 중복 방지 테스트")
+        inner class IncreaseViewCountTest {
+            @BeforeEach
+            fun cacheClear() {
+                cacheManager.getCache("activityViewCount")?.clear()
+            }
 
         @Test
         @DisplayName("[성공] 2개의 다른 ip + user-agent 에서 짧은 시간 내에 대량의 요청을 날려도 조회수에 제한이 걸린다")
@@ -1324,25 +1495,26 @@ class ActivityIntegrationTest : IntegrationTest() {
                 )
             assertThat(given.viewCount).isEqualTo(0)
 
-            // when
-            var requestCount = 0
-            val userAgents = listOf("Test1", "Test2")
-            val repeatCnt = 30
+                // when
+                var requestCount = 0
+                val userAgents = listOf("Test1", "Test2")
+                val repeatCnt = 30
 
-            userAgents.forEach { userAgent ->
-                repeat(repeatCnt) {
-                    requestCount++
-                    mockMvc
-                        .post("/v1/activities/${given.id}/view") {
-                            header("User-Agent", userAgent)
-                        }.andExpect { status { isOk() } }
+                userAgents.forEach { userAgent ->
+                    repeat(repeatCnt) {
+                        requestCount++
+                        mockMvc
+                            .post("/v1/activities/${given.id}/view") {
+                                header("User-Agent", userAgent)
+                            }.andExpect { status { isOk() } }
+                    }
                 }
-            }
 
-            // then
-            val updated = activityRepository.findById(given.id).get()
-            assertThat(requestCount).isEqualTo(userAgents.size * repeatCnt)
-            assertThat(updated.viewCount).isEqualTo((userAgents.size * MAX_VIEW_ATTEMPTS).toLong())
+                // then
+                val updated = activityRepository.findById(given.id).get()
+                assertThat(requestCount).isEqualTo(userAgents.size * repeatCnt)
+                assertThat(updated.viewCount).isEqualTo((userAgents.size * MAX_VIEW_ATTEMPTS).toLong())
+            }
         }
     }
 }
