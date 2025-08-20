dca02fe8377de05f39d2372057f2d2a011290fb4
dnjsals45 <dnjsals45@naver.com>
2025-08-20 10:28:32 +0900

    🐛fix: Dto 객체 Schema 설정 추가

--- src/main/kotlin/picklab/backend/common/file/entrypoint/response/CreatePresignedurlResponse.kt
+++ src/main/kotlin/picklab/backend/common/file/entrypoint/response/CreatePresignedurlResponse.kt
@@ -1,6 +1,10 @@
 package picklab.backend.common.file.entrypoint.response
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class CreatePresignedurlResponse(
+    @Schema(description = "PUT요청용 Presgined Url")
     val presignedUrl: String,
+    @Schema(description = "이미지 업로드 완료 시 접근 가능한 Public URL")
     val publicReadUrl: String,
 )
