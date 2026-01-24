3023c5eebac1cff7ac787e8e978a10627285d0f8
hwk9407 <wonhur5@gmail.com>
2026-01-24 10:19:43 +0900

    ♻️ refactor: Swagger 문서화를 위한 validation 어노테이션 타겟 명시
    - 리뷰, 직무, 알림, 멤버, 파일 도메인에 누락된 타겟 명시

--- src/main/kotlin/picklab/backend/file/entrypoint/response/CreatePresignedurlResponse.kt
+++ src/main/kotlin/picklab/backend/file/entrypoint/response/CreatePresignedurlResponse.kt
@@ -3,6 +3,6 @@ package picklab.backend.file.entrypoint.response
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class CreatePresignedurlResponse(
-    @Schema(description = "PUT요청용 Presgined Url")
+    @field:Schema(description = "PUT요청용 Presgined Url")
     val presignedUrl: String,
 )
